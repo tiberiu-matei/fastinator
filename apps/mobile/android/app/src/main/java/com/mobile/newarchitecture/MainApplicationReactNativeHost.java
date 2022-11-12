bcdad7900a1b269f0693b7a8befc077e081a879b
Tibi <matei.tiberiu02@gmail.com>
2022-11-12 13:06:06 +0000

    CI

--- apps/mobile/android/app/src/main/java/com/mobile/newarchitecture/MainApplicationReactNativeHost.java
+++ apps/mobile/android/app/src/main/java/com/mobile/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.mobile.newarchitecture;
+package com.fastinator.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.mobile.BuildConfig;
-import com.mobile.newarchitecture.components.MainComponentsRegistry;
-import com.mobile.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.fastinator.BuildConfig;
+import com.fastinator.newarchitecture.components.MainComponentsRegistry;
+import com.fastinator.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- apps/mobile/android/app/src/main/java/com/mobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ apps/mobile/android/app/src/main/java/com/mobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.mobile.newarchitecture.modules;
+package com.fastinator.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
