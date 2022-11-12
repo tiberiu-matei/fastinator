c8ce53c2d1ef46cc6af90645b19ead0947fb4d77
Tibi <matei.tiberiu02@gmail.com>
2022-11-12 21:41:39 +0000

    Rename native apps to "fastinator"

--- apps/mobile-e2e/src/app.spec.ts
+++ apps/mobile-e2e/src/app.spec.ts
@@ -1,11 +1,11 @@
 import { device, element, by, expect } from 'detox';
 
-describe('Mobile', () => {
+describe('Fastinator', () => {
     beforeEach(async () => {
         await device.reloadReactNative();
     });
 
     it('should display welcome message', async () => {
-        await expect(element(by.id('heading'))).toHaveText('Welcome Mobile 👋');
+        await expect(element(by.id('heading'))).toHaveText('Welcome Fastinator 👋');
     });
 });
--- apps/mobile/ios/FastinatorTests/FastinatorTests.m
+++ apps/mobile/ios/FastinatorTests/FastinatorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MobileTests : XCTestCase
+@interface FastinatorTests : XCTestCase
 
 @end
 
-@implementation MobileTests
+@implementation FastinatorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
