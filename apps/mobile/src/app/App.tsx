d78a4d94033962d41857bff6b3a615a2ecc047f9
Tiberiu Matei <matei.tiberiu02@gmail.com>
2024-09-18 07:06:58 +0100

    mary

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
     return (
         <View>
-            <Text testID="Fastinator">Fastinator</Text>
+            <Text testID="Fastinator">Mary</Text>
         </View>
     );
 }
